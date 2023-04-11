b6664f4801fdfdc3206634171ef226f597a6e807
tacocat199 <farteezy19@gmail.com>
2023-04-11 23:57:03 +0700

    complete-ish

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
-const Discord = require('discord.js');
+require('dotenv').config();
+
+const { Client, GatewayIntentBits } = require('discord.js');
 const qrcode = require('qrcode');
 const qrimage = require('qr-image');
 
-const bot = new Discord.Client();
+const bot = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.DirectMessages, GatewayIntentBits.MessageContent] });
 
-bot.on('message', async message => {
+bot.on('messageCreate', async message => {
     // Check if the message starts with the command
     if (message.content.startsWith('!qr')) {
         // Extract the argument from the message
         const argument = message.content.split(' ')[1];
+        if (typeof argument !== 'string') {
+            return message.channel.send('Please provide a valid input.');
+        }
+        // Send a response message with the input message content
+        await message.channel.send(`Generating QR code for ${argument}...`);
         // Generate the QR code from the argument
         const qrCode = await qrcode.toBuffer(argument);
         // Convert the buffer to an image file
-        const qrImage = qrimage.imageSync(qrCode, { type: 'png '});
+        const qrImage = qrimage.imageSync(qrCode, { type: 'png'});
         // Send the image file as an attachment in a new message
-        await message.channel.send(new Discord.MessageAttachment(qrImage, 'qr-code.png'));
+        await message.channel.send({ files: [qrImage] });
     }
 });
 
-bot.login('MTA5NTM0NzAxNjM5MDkzODc2OQ.G7FHMz.9kF5YyrOEDm6qoQP48CH-YVrXEL-tGLryFrswU');
+bot.login(process.env.BOT_TOKEN);
