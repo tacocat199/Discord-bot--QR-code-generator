7b2abb3e07d5f5202fcdbd2e5adb3d92a2e1a056
tacocat199 <farteezy19@gmail.com>
2023-04-12 11:36:31 +0700

    adding slash command

--- index.js
+++ index.js
@@ -6,24 +6,61 @@ const qrimage = require('qr-image');
 
 const bot = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.DirectMessages, GatewayIntentBits.MessageContent] });
 
-bot.on('messageCreate', async message => {
-    console.log(message);
-    // Check if the message starts with the command
-    if (message.content.startsWith('!qr')) {
-        // Extract the argument from the message
-        const argument = message.content.split(' ')[1];
-        if (typeof argument !== 'string') {
-            return message.channel.send('Please provide a valid input.');
-        }
-        // Send a response message with the input message content
-        await message.channel.send(`Generating QR code for ${argument}...`);
-        // Generate the QR code from the argument
-        const qrCode = await qrcode.toBuffer(argument);
-        // Convert the buffer to an image file
-        const qrImage = qrimage.imageSync(qrCode, { type: 'png'});
-        // Send the image file as an attachment in a new message
-        await message.channel.send({ files: [qrImage] });
+bot.on('ready', () => {
+    console.log(`Logged in as ${bot.user.tag}!`);
+
+    // Define the slash command
+    const command = {
+        name: 'qr',
+        description: 'Generate a QR code',
+        options: [
+            {
+                name: 'input',
+                description: 'The input to generate the QR code from',
+                type: 3,
+                required: true
+            }
+        ]
+    };
+
+    // Register the slash command with Discord
+    bot.application.commands.create(command);
+});
+
+bot.on('interactionCreate', async (interaction) => {
+    if (!interaction.isCommand()) return;
+
+    const { commandName, options } = interaction;
+
+    if (commandName == 'qr') {
+        const input = options.getString('input');
+
+        await interaction.reply(`Generating QR code for ${input}...`);
+
+        const qrCode = await qrcode.toBuffer(input);
+        const qrImage = qrimage.imageSync(qrCode, { type: 'png' });
+
+        await interaction.followUp({ files: [qrImage] });
     }
+});
+
+bot.on('messageCreate', async (message) => {
+    if (!message.content.startsWith('!qr')) return;
+
+    const input = message.content.slice(4);
+
+    if (!input) {
+        return message.reply('Please provide input to generate the QR code from!');
+    }
+
+    const qrCode = await qrcode.toBuffer(input);
+    const qrImage = qrimage.imageSync(qrCode, {
+        type: 'png'
+    });
+
+    await message.channel.send({
+        files: [qrImage]
+    });
 });
 
 bot.login(process.env.BOT_TOKEN);
